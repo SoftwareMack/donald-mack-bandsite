5c16dd63ec1d4801b26d4e68bf62941308b5ce94
Donald Mack <142954704+SoftwareMack@users.noreply.github.com>
2023-12-13 15:25:16 -0500

    Update build-shows-page.js. eite some changes

--- donald-mack-bandsite/scripts/build-shows-page.js
+++ donald-mack-bandsite/scripts/build-shows-page.js
@@ -1,53 +1,81 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    const showsData = [
-      { date: 'Mon Sept 06 2021', venue: 'Ronald Lane', location: 'San Francisco, CA' },
-    ];
+  const showsData = [
+    {
+      date: 'Mon Sept 06 2021',
+      venue: 'Ronald Lane',
+      location: 'San Francisco, CA',
+    },
+  
+  ];
 
+  function renderShows() {
+    const mainSection = document.querySelector('.main');
 
-    function createTicket(show) {
-      const ticketContainer = document.querySelector('.ticket-container');
-
+    showsData.forEach((show, index) => {
       const ticket = document.createElement('div');
       ticket.classList.add('ticket');
+      ticket.id = `ticket${index + 1}`;
 
-      const dateDesktop = document.createElement('p');
-      dateDesktop.classList.add('date', 'desktop');
-      dateDesktop.textContent = show.date;
+      ['date', 'venue', 'location'].forEach(key => {
+        const label = document.createElement('span');
+        label.classList.add('label');
+        label.textContent = key.toUpperCase();
 
-      const dateMobile = document.createElement('p');
-      dateMobile.classList.add('date', 'mobile');
-      dateMobile.textContent = formatMobileDate(show.date);
+        const content = document.createElement('span');
+        content.classList.add('content');
+        content.textContent = show[key];
 
-      const venue = document.createElement('p');
-      venue.classList.add('venue');
-      venue.textContent = show.venue;
+        const infoContainer = document.createElement('div');
+        infoContainer.classList.add(key);
+        infoContainer.appendChild(label);
+        infoContainer.appendChild(content);
 
-      const location = document.createElement('p');
-      location.classList.add('location');
-      location.textContent = show.location;
+        ticket.appendChild(infoContainer);
+      });
 
-      const buyTicketsButton = document.createElement('button');
-      buyTicketsButton.classList.add('buy-tickets-cta');
-      buyTicketsButton.textContent = 'BUY TICKETS';
+      const ctaButton = document.createElement('button');
+      ctaButton.classList.add('cta');
+      ctaButton.textContent = 'BUY TICKETS';
+      ctaButton.addEventListener('click', function (event) {
+        event.stopPropagation();
+        alert(`Buy Tickets Clicked for ${ticket.id}`);
+ 
+      });
 
-      ticket.appendChild(dateDesktop);
-      ticket.appendChild(dateMobile);
-      ticket.appendChild(venue);
-      ticket.appendChild(location);
-      ticket.appendChild(buyTicketsButton);
+      ticket.appendChild(ctaButton);
 
-      ticketContainer.appendChild(ticket);
-    }
+    
+      ticket.addEventListener('click', function () {
+        handleTicketClick(ticket.id);
+      });
+
+
+      mainSection.appendChild(ticket);
+    });
+  }
+
+  function handleTicketClick(ticketId) {
+  
+    resetTickets();
+
+    const clickedTicket = document.getElementById(ticketId);
+    clickedTicket.classList.add('selected');
+  }
 
  
-    function formatMobileDate(dateString) {
-      const dateArray = dateString.split(' ');
-      return `${dateArray[1]} ${dateArray[2]}, ${dateArray[3]}`;
-    }
-
-
-    showsData.forEach((show) => {
-      createTicket(show);
+  function resetTickets() {
+    const allTickets = document.querySelectorAll('.ticket');
+    allTickets.forEach(ticket => {
+      ticket.classList.remove('selected');
     });
-  });
+  }
+
+  renderShows();
+
+
+});
+
+     
+
+ 
